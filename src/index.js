bad43687b49f965db35b9891bbe26851dbc7026c
Ehsan Farahi <ehsanullah.farahi12@gmail.com>
2023-10-10 20:33:52 +0200

    further work on footer 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./Components/App.js";
 
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 // import "https://kit.fontawesome.com/ac6052ecd7.js";
 import "./styles/index.css";
 
